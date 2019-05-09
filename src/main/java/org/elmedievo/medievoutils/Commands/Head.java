635bb07c1a9a386e40e8f229fba5322bfb7527f4
BGM <bgmp00@protonmail.com>
2019-05-09 00:29:18 -0300

    Fix some permission node parameters

--- src/main/java/org/elmedievo/medievoutils/Commands/Head.java
+++ src/main/java/org/elmedievo/medievoutils/Commands/Head.java
@@ -11,8 +11,7 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
 import org.elmedievo.medievoutils.MedievoUtils;
 
-import static org.elmedievo.medievoutils.util.Generic.TOO_FEW_ARGS;
-import static org.elmedievo.medievoutils.util.Generic.TOO_MANY_ARGS;
+import static org.elmedievo.medievoutils.util.Generic.*;
 
 public class Head implements CommandExecutor {
 
@@ -25,21 +24,27 @@ public class Head implements CommandExecutor {
     @SuppressWarnings("deprecation")
     @Override
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (command.getName().equalsIgnoreCase("head") && sender instanceof Player) {
-            if (args.length == 1) {
-                Player player = (Player) sender;
-                String skullOwner = args[0];
-                ItemStack skullItem = new ItemStack(Material.SKULL_ITEM, 1, (short) SkullType.PLAYER.ordinal());
-                SkullMeta skullMeta = (SkullMeta) skullItem.getItemMeta();
-                skullMeta.setOwner(skullOwner);
-                skullItem.setItemMeta(skullMeta);
-                player.getInventory().addItem(skullItem);
-                player.sendMessage(ChatColor.AQUA + "Given " + ChatColor.YELLOW + args[0] + ChatColor.AQUA + "'s head");
-            } else if (args.length < 1) {
-                sender.sendMessage(TOO_FEW_ARGS);
+        if (command.getName().equalsIgnoreCase("head") && sender.hasPermission("medievo.utils.head")) {
+            if (sender instanceof Player) {
+                if (args.length == 1) {
+                    Player player = (Player) sender;
+                    String skullOwner = args[0];
+                    ItemStack skullItem = new ItemStack(Material.SKULL_ITEM, 1, (short) SkullType.PLAYER.ordinal());
+                    SkullMeta skullMeta = (SkullMeta) skullItem.getItemMeta();
+                    skullMeta.setOwner(skullOwner);
+                    skullItem.setItemMeta(skullMeta);
+                    player.getInventory().addItem(skullItem);
+                    player.sendMessage(ChatColor.AQUA + "Given " + ChatColor.YELLOW + args[0] + ChatColor.AQUA + "'s head");
+                } else if (args.length < 1) {
+                    sender.sendMessage(TOO_FEW_ARGS);
+                } else {
+                    sender.sendMessage(TOO_MANY_ARGS);
+                }
             } else {
-                sender.sendMessage(TOO_MANY_ARGS);
+                sender.sendMessage(NO_CONSOLE);
             }
+        } else {
+            sender.sendMessage(NO_PERMISSION);
         }
         return true;
     }
